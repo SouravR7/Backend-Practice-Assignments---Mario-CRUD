a3d2ce479486b59954cbb22fc4b94eaa60bb22d9
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-16 13:09:52 +0530

    changessssssss

--- src/app.js
+++ src/app.js
@@ -1,45 +1,24 @@
 const express = require('express')
 const app = express()
-const bodyParser = require("body-parser");
 const marioModel = require('./models/marioChar');
 
-// Middlewares
-app.use(express.urlencoded());
-
-// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-// your code goes here
 
 app.get("/mario", (req,res)=>{
 
-     let characters = await marioModel.find({});
-     res.send(characters);
-
-    marioModel.find({}, (err, data) => {
-        if(err){
-            res.status(400).json({"message": error.message});
-        }
-        else{
-            res.json(data);
-        }
-    });
+    marioModel.find().then(data => res.send(data))
+    .catch(error => res.send(error));
+    return;
 });
 
 app.get("/mario/:id", (req,res)=>{
-
-    marioModel.findOne({
-        _id: id,
-    }).then((err,data) => {
-        if (err) {
-            res.status(400).json({"message": error.message});
-        } else{
-            res.json(data);
-        }
-    });
-});
+    marioModel.findById(req.params.id)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })});
 
 
 app.post("/mario",(req,res)=>{
@@ -56,69 +35,25 @@ app.post("/mario",(req,res)=>{
         weight
     })
     mario.save();
-    // console.log(name);
-    // console.log(weight);
-    // console.log(mario);
-    res.status(201).send(mario);
+
+    res.send(mario);
 })
 
-app.patch("/mario/:id",async (req,res)=>{
-    
-    try {
-
-        let id = req.params.id;
-        let updateObject = req.body;
-
-        let check = await marioModel.find({_id :id});
-
-         if(check.length === 0) throw new Error;
-
-        await marioModel.update(
-            { _id: id },
-            {$set: updateObject},
-            {returnOriginal: false}
-        );
-
-        let data = await marioModel.findById({_id:id})
-
-        // console.log(updateObject);
-        // console.log(data);
-
-        res.status(201).send(data);
-        
-    } catch (error) {
-        res.status(400).send({message: error.message});
-    }
+app.patch("/mario/:id", (req, res) => {
+    marioModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(400).json({"message" : err.message}))
 })
 
-app.delete("/mario/:id",async (req,res)=>{
-    
-    try {
-
-        let id = req.params.id;
-
-        let data = await marioModel.findById({_id:id});
-
-        if(!data){
-             res.status(400).send({message: "error.message"});
-            return;
-            throw new error;
-        }
-
-        await marioModel.deleteOne(
-            { _id: id }
-        );
-
-        // console.log(updateObject);
-        // console.log(data);
-
-        res.status(200).send({message: 'character deleted'});
-        
-    } catch (error) {
-        res.status(400).send({message: error.message});
-    }
+app.delete("/mario/:id", (req, res) => {
+    marioModel.deleteOne({_id: req.params.id})
+    .then(data => {
+        return res.status(200).send({"message":"character deleted"})
+    })
+    .catch(err => res.status(400).json({"message":err.message}))
 })
 
 
 
 module.exports = app;
+
